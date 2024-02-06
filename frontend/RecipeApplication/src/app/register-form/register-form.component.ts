9f5fce03292cc7b83ebfddb33938587deb551e84
Radu Condrat <condrat.radu69@yahoo.com>
2024-02-06 19:14:30 +0200

    API update

---
+++ frontend/RecipeApplication/src/app/register-form/register-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../User/user';
+import { UserService } from '../User/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+export class RegisterFormComponent implements OnInit {
+  public users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    //this.getUsers();
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+        console.log(this.users);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onRegisterUser(registerForm: NgForm): void {
+    console.log(registerForm.value)
+    this.userService.addUser(registerForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
