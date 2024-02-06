63f11205ea581595743b400fab23df10ab34d6cf
Radu Condrat <condrat.radu69@yahoo.com>
2024-02-06 19:16:28 +0200

    Register form added

--- frontend/RecipeApplication/src/app/app.module.ts
+++ frontend/RecipeApplication/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RecipeService } from './recipe.service';
+import { RecipeService } from './Recipe/recipe.service';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [RecipeService],
   bootstrap: [AppComponent],
